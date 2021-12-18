50c1c125eaf80274fc32821a1bdf2ffc04025516
juanwmedia <juan@wmedia.es>
2021-12-18 14:12:48 +0100

    All done!

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,33 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+const store= createStore({
   state: {
+    images: [],
   },
   mutations: {
+    setImages(state, images) {
+      state.images = images;
+    },
+    addImages(state, images) {
+      state.images = state.images.concat(images)
+    }
+  },
+  getters: {
+    getImage: (state) => (imageID) => state.images.find(image => image.id === imageID)
   },
   actions: {
+    loadFirstImages({ commit }) {
+      return new Promise(async (resolve) => {
+        const request = await fetch(
+          "https://picsum.photos/v2/list?limit=10&page=1"
+        );
+        const images = await request.json();
+        commit("setImages", images);
+        resolve();
+      });
+    },
   },
-  modules: {
-  }
 })
+
+export default store
+export const checkImagesLoaded = store.dispatch("loadFirstImages");
